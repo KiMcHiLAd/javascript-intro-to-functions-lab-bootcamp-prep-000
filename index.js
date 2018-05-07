a4f86e6cf499a6cac6fdd7c8859b5f2d217d1a12
Hania Bigo <hrbigos@umich.edu>
2018-05-07 19:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  
+  console.log(string.toUpperCase()) 
   
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 
+function sayHiToGrandma(string) {
+  
+  if (string === lowercase)
+  return("I can\'t hear you!")
+}
 
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
